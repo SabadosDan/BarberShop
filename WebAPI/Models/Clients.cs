3ba8cb5ad7295a6297c1fd82aa3a1fb11bd34588
Sabados Dan <91564800+SabadosDan@users.noreply.github.com>
2025-01-04 15:09:11 +0200

    first commit

---
+++ WebAPI/Models/Clients.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Models
+{
+    public class Clients
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
